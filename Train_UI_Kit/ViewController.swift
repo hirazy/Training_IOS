d320a845df18bf549cdef34934eb5d0aa3a8aae0
nang.le@monstar-lab.com <nang.le@monstar-lab.com>
2022-10-24 12:48:12 +0700

    Initial Commit

---
+++ Train_UI_Kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Train_UI_Kit
+//
+//  Created by Nang Le D. VN.Hanoi on 24/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
