02747e20854c8d2997812716c5ef3b6c977d808d
nang.le@monstar-lab.com <nang.le@monstar-lab.com>
2022-10-31 16:49:51 +0700

    init

--- Train_UI_Kit/ViewController.swift
+++ Train_UI_Kit/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
+    
+    @IBOutlet weak var textFielld: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: 110,height: textFielld.frame.size.height))
+        label.text = "Input number"
+        
+        textFielld.leftView = label
+        textFielld.leftViewMode = .always
     }
 
-
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        print(sender.value);
+    }
+    
+    @IBAction func handleClickBtn(_ sender: UIButton) {
+        let text = textFielld.text ?? ""
+        print("textField value = \(text)")
+    }
 }
 
